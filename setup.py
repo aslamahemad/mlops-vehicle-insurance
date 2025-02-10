8e30f41fd4f75f337a22f16cc15f18441a326112
Aslam <aslamahemad@gmail.com>
2025-02-10 16:55:58 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aslam Ahemad",
+    author_email="aslamahemad@gmail.com",
+    packages=find_packages()
+)
